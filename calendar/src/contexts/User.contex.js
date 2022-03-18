09b1e699e72af81604da1a7a3dfd5df9dabc9062
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-03-18 12:05:56 -0300

    [build] add logic to functions at user context

--- calendar/src/contexts/User.contex.js
+++ calendar/src/contexts/User.contex.js
@@ -1,23 +1,64 @@
 import React, { createContext, useState } from "react";
+import { Auth } from "aws-amplify";
 
 export const UserContext = createContext({
   user: null,
   signin: () => {},
   logout: () => {},
   signup: () => {},
+  resendConfirmationCode: () => {},
 });
 
 export default function UserProvider({ children }) {
   const [user, setUser] = useState();
 
-  function signin() {}
+  async function signin({ username, password }) {
+    try {
+      const user = await Auth.signIn(username, password);
+      console.log(user);
+    } catch (err) {
+      console.log("error signing in", err);
+      throw err;
+    }
+  }
 
-  function logout() {}
+  async function signup({ username, password, attributes: { email } }) {
+    try {
+      const { user, userConfirmed, userSub } = await Auth.signUp({
+        username,
+        password,
+        attributes: { email },
+      });
+      setUser({ username: user.getUsername(), userConfirmed, userSub });
+    } catch (err) {
+      setUser(null);
+      throw err;
+    }
+  }
 
-  function signup() {}
+  async function resendConfirmationCode() {
+    try {
+      await Auth.resendSignUp(user.username);
+      console.log("code resent successfully");
+    } catch (err) {
+      console.log("error resending code: ", err);
+      throw err;
+    }
+  }
+
+  async function signOut() {
+    try {
+      await Auth.signOut();
+    } catch (err) {
+      console.log("error signing out: ", err);
+      throw err;
+    }
+  }
 
   return (
-    <UserContext.Provider value={{ user, signin, logout, signup }}>
+    <UserContext.Provider
+      value={{ user, signin, signOut, signup, resendConfirmationCode }}
+    >
       {children}
     </UserContext.Provider>
   );
