9504ca72d644e2ab0ebec05b68922c5f7f6096c2
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-03-21 11:37:40 -0300

    isolate routes from app

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,47 +1,15 @@
 import React from "react";
-import { useRoutes, Navigate } from "react-router-dom";
 
 import "styles/style.scss";
 
 import UserProvider from "contexts/User.context";
-import { NavOutlet, AuthOutlet } from "outlets";
-import { Home, SignIn, MyCalendars } from "./pages/index";
+
+import Routes from "./Routes";
 
 export default function App() {
-  function multipleRoutes({ routes = [], element, ...props }) {
-    return routes.map((path) => ({ path, element, ...props }));
-  }
-
-  const routes = useRoutes([
-    {
-      path: "/",
-      element: <NavOutlet />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "my-calendars",
-          element: <MyCalendars />,
-        },
-      ],
-    },
-    {
-      path: "auth",
-      element: <AuthOutlet />,
-      children: [
-        {
-          path: "login",
-          element: <SignIn />,
-        },
-        ...multipleRoutes({
-          routes: ["", "*"],
-          element: <Navigate to="login" />,
-        }),
-      ],
-    },
-  ]);
-
-  return <UserProvider>{routes}</UserProvider>;
+  return (
+    <UserProvider>
+      <Routes />
+    </UserProvider>
+  );
 }
---
+++ calendar/src/Routes.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useRoutes, Navigate } from "react-router-dom";
+
+import { NavOutlet, AuthOutlet } from "outlets";
+import { Home, SignIn, SignUp, MyCalendars } from "./pages/index";
+
+export default function Routes() {
+  function multipleRoutes({ routes = [], element, ...props }) {
+    return routes.map((path) => ({ path, element, ...props }));
+  }
+
+  return useRoutes([
+    {
+      path: "/",
+      element: <NavOutlet />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "my-calendars",
+          element: <MyCalendars />,
+        },
+      ],
+    },
+    {
+      path: "auth",
+      element: <AuthOutlet />,
+      children: [
+        {
+          path: "signin",
+          element: <SignIn />,
+        },
+        {
+          path: "signup",
+          element: <SignUp />,
+        },
+        ...multipleRoutes({
+          routes: ["", "*"],
+          element: <Navigate to="signin" />,
+        }),
+      ],
+    },
+  ]);
+}
