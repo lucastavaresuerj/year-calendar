942d1f5643c98450541dad1b06249f3c88bcd3c5
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-03-25 15:07:59 -0300

    Adding flow for the case when there is no schema file at bucket

--- spec-scripts/template/compare-and-change-params.sh
+++ spec-scripts/template/compare-and-change-params.sh
@@ -2,11 +2,19 @@
 # S3_BUCKET_NAME=saves-bucket-calendar-year-savesbucket-nx3ap2uftsmb
 # CODEBUILD_BUILD_NUMBER=1
 
-aws s3 sync \
-  s3://${S3_BUCKET_NAME}/${GRAPHQL_FOLDER_PATH}/ \
-  ./${GRAPHQL_FOLDER_PATH}/ \
-  --exclude "*" \
-  --include "schema-*.graphql"
+{ # Try
+  aws s3 sync \
+    s3://${S3_BUCKET_NAME}/${GRAPHQL_FOLDER_PATH}/ \
+    ./${GRAPHQL_FOLDER_PATH}/ \
+    --exclude "*" \
+    --exclude "*/" \
+    --include "schema-*.graphql" | \
+    grep "download"
+} || { #Catch
+  echo "There is no file with name like 'schema-*.graphql'"
+  echo "Ending script"
+  exit 1
+}
 
 { # Try
   diff ./${GRAPHQL_FOLDER_PATH}/schema-*.graphql
