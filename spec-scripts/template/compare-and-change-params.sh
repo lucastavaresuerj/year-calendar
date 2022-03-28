5b40f1bbe6c95d09e5164d19478bff68d7ca892f
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-03-28 15:31:43 -0300

    [template] making more descriptive what is doing after the diff command

--- spec-scripts/template/compare-and-change-params.sh
+++ spec-scripts/template/compare-and-change-params.sh
@@ -23,20 +23,27 @@
 	case $ERRO_CODE in
 	0) { 
 		echo "GraphQL schemas are equals, there is no need to update"
+    echo "Deleting new schema"
     rm ./${GRAPHQL_FOLDER_PATH}/schema-${CODEBUILD_BUILD_NUMBER}.graphql
 	};;
 	1) { 
 		echo "Need to update"
     echo "Deleting old schema"
-    find ./${GRAPHQL_FOLDER_PATH}/ -name "*.graphql" ! -name "*${CODEBUILD_BUILD_NUMBER}.graphql" -delete
+    find ./${GRAPHQL_FOLDER_PATH}/ \
+      -name "*.graphql" \
+      -not -name "*${CODEBUILD_BUILD_NUMBER}.graphql" \
+      -delete \
+      -print
     echo "Changing parameter GraphQLSchemaFileName"
     cat cfn-templates/parameters.json | \
       fx ".map($(cat cfn-templates/mapParams.js))" \
       > cfn-templates/parameters.json
 	};;
 	2) { 
-		echo "Diff params error,\n Listing files like 'schema-.*\.graphql'"
-    ls ./${GRAPHQL_FOLDER_PATH} | grep 'schema-.*\.graphql'
+		echo "Diff params error, \nListing files like 'schema-*.graphql'"
+    find ./${GRAPHQL_FOLDER_PATH}/ \
+      -name "schema-*.graphql" \
+      -print
 	};;
 	*) {
 		echo "Something went wrong with exit code: $?"
