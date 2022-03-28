5f2e5510b4944046b81f1eb71418f5b6c5352bb0
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-03-28 10:51:55 -0300

    [template] add query type definition and add more complexity when analising the diff

--- spec-scripts/template/compare-and-change-params.sh
+++ spec-scripts/template/compare-and-change-params.sh
@@ -14,22 +14,34 @@
   echo "There is no file with name like 'schema-*.graphql'"
   echo "Ending script"
   exit 1
-}
+} || exit
 
 { # Try
-  diff ./${GRAPHQL_FOLDER_PATH}/schema-*.graphql
-} && { # Do if has no error at try
-  echo "GraphQL schemas are equals, there is no need to update"
-  rm ./${GRAPHQL_FOLDER_PATH}/schema-${CODEBUILD_BUILD_NUMBER}.graphql
-} || { # Catch
-  echo "Need to update"
-  aws s3 rm \
-    s3://saves-bucket-calendar-year-savesbucket-nx3ap2uftsmb/cfn-templates/graphQL/ \
-    --exclude "*" \
-    --include "schema-*.graphql" \
-    --recursive
-  echo "Changing parameter GraphQLSchemaFileName"
-  cat cfn-templates/parameters.json | \
-    fx ".map($(cat cfn-templates/mapParams.js))" \
-    > cfn-templates/parameters.json
+	diff ./${GRAPHQL_FOLDER_PATH}/schema-*.graphql
+	ERRO=$?
+} && { # Catch with switch/case and exit code
+	case $ERRO in
+	0) { 
+		echo "GraphQL schemas are equals, there is no need to update"
+    rm ./${GRAPHQL_FOLDER_PATH}/schema-${CODEBUILD_BUILD_NUMBER}.graphql
+	};;
+	1) { 
+		echo "Need to update"
+    aws s3 rm \
+      s3://${S3_BUCKET_NAME}/${GRAPHQL_FOLDER_PATH}/ \
+      --exclude "*" \
+      --include "schema-*.graphql" \
+      --recursive
+    echo "Changing parameter GraphQLSchemaFileName"
+    cat cfn-templates/parameters.json | \
+      fx ".map($(cat cfn-templates/mapParams.js))" \
+      > cfn-templates/parameters.json
+	};;
+	2) { 
+		echo "Missing diff params, some file was not found"
+	};;
+	*) {
+		echo "Something went wrong with exit code: $?"
+	};;
+	esac
 }
