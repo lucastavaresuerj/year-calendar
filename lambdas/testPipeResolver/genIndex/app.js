fcd587caf992c5bd1ba6cde02b249992f4a82239
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-04-01 15:59:08 -0300

    [lambda] trying to fix null value to resolver

--- lambdas/testPipeResolver/genIndex/app.js
+++ lambdas/testPipeResolver/genIndex/app.js
@@ -2,16 +2,17 @@ exports.handler = async (event) => {
   const {
     appSyncContext: {
       identity: { username },
-      arguments: {
-        input: { name },
-      },
       arguments,
     },
   } = event;
+  const {
+    input: { name },
+  } = arguments;
+
   const random = (Math.random() * 1000000).toFixed(0);
 
   return {
-    ...arguments,
+    ...arguments["input"],
     index: `${username}-${name}-${random}`,
   };
 };
